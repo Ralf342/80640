04347f81d2af1d687094b209d127c9f179769ba6
Ralf342 <j.oskar.v282143@gmail.com>
2023-10-19 12:55:04 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,4 +1,5 @@
 package mx.uv;
+import static spark.Spark.*;
 
 /**
  * Hello world!
@@ -8,6 +9,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Mundo!" );
+        // System.out.println( "Hola Mundo!" );
+        get("/",(request,response)->"<h1>Hola mundo!</h1>");
+        get("/ruta1",(request,response)->"<h1>Adios mundo!</h1>");
+        get("/ruta2",(request,response)->"<h1>Que tal mundo!</h1>");
+        get("/ruta3",(request,response)->"{'alumno':'Jhon','Matricula':'a001','carrera':'TC'}");
     }
 }
